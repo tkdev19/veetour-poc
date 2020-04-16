e38230459cb7dfe2bc17feff6a1526cf17099db1
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-16 14:27:38 +0700

    Edit Home

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import Head from "next/head";
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <h1 className="text-center">Title</h1>
+    <h1 className="text-center">Home</h1>
     <div className="row">
       <div className="col-6">
         Column 1
